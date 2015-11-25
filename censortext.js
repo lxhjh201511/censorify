9dee03f7163b8530d9dde5749f53136cab64e63b
liangxiaohui <920957398@qq.com>
2015-11-25 15:21:50 +0800

    代码提交

---
+++ censortext.js
@@ -0,0 +1,23 @@
+/**
+ * Created by xiaohui on 2015/11/25.
+ */
+var censoredWords=["sad","bad","mad"];
+var customCensoredWords=[];
+function censor(inStr){
+    for (idx in customCensoredWords){
+        inStr=inStr.replace(censoredWords[idx],"****");
+    }
+    for (idx in customCensoredWords){
+        inStr=instr.replace(customCensoredWords[idx],"****");
+    }
+    return instr;
+}
+function addCensoredWord(word){
+    customCensoredWords.push(word);
+}
+function getCensorWords(){
+    return censoredWords.concat(customCensoredWords);
+}
+exports.censor=censor;
+exports.addCensoredWord=addCensoredWord;
+exports.getCensoredWords=getCensorWords;
